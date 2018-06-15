ae4961ae012472dea804dd176eed229edc51544f
Aluno <Aluno@SN-351145.lan>
2018-06-14 21:10:02 -0300

    update

--- src/main/java/br/senai/sc/edu/projetomaria/io/ProdutoReader.java
+++ src/main/java/br/senai/sc/edu/projetomaria/io/ProdutoReader.java
@@ -17,52 +17,32 @@ import br.senai.sc.edu.projetomaria.model.Produto;
 
 
 public class ProdutoReader {
-	
 	private static final Logger LOGGER = LogManager.getLogger();
+	//bdd1
 	
-	public void ler(Path caminho){
-		LOGGER.info("aqui");
-		
+	public void cargaInicial(Path caminho){
 		ArrayList<Produto> produtos = new ArrayList<>();   
-	     Produto novoProduto = null;
-	     int erros = 0;
-	     int cont = 0;
-	     
+	     Produto novoProduto = null; 
 	      try (
 	          Reader leitor = Files.newBufferedReader(caminho);
 	          CSVParser conversor = new CSVParser(leitor, CSVFormat.DEFAULT);
 	          )
 	      {
 	          for(CSVRecord gravar : conversor){
-	              cont++;
 	              String sku = gravar.get(0);
 	              String nomeProduto = gravar.get(1);
 	              String idFamiliaComercial = gravar.get(2);                                              
 	              
-	              boolean  skuR = gravar.get(0).matches("[0-9]{1,20}");
-	              boolean  nomeProdutoR = gravar.get(1).matches("[A-zÀ-ú0-9 '´]{1,255}");
-	              boolean  idFamiliaComercialR = gravar.get(2).matches("[0-9]{1,20}");                                  
-	              
-	              if(skuR == false || nomeProdutoR == false || idFamiliaComercialR == false){
-	                  erros++;
-	                  System.out.println("Há um erro na linha "+cont+".");
-	              }else{
-	                  novoProduto = new Produto();
-	                  novoProduto.setSku(Integer.parseInt(sku));
-	                  novoProduto.setDescricao(nomeProduto);
-	                  novoProduto.setIdComercial(Integer.parseInt(idFamiliaComercial));
-	              }	              
-	              produtos.add(novoProduto);              
-	          }
-	          if(erros == 0){
-	              System.out.println("Arquivo validado e importado para a base.");
-	              ProdutoDAO dao = new ProdutoDAO();               
-	              dao.salvarProdutos(produtos); 
-	          }
+                  novoProduto = new Produto();
+                  novoProduto.setSku(Integer.parseInt(sku));
+                  novoProduto.setDescricao(nomeProduto);
+                  novoProduto.setIdComercial(Integer.parseInt(idFamiliaComercial));
+                  produtos.add(novoProduto);
+	          }	              
+              ProdutoDAO dao = new ProdutoDAO();               
+              dao.salvarProdutos(produtos); 
 	      } catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			LOGGER.info("erro");
 		}   
 	} 
 }
--- src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
+++ src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
@@ -33,7 +33,7 @@ public class CargaService {
 
 	public void insertProduto(Path path) {
 		ProdutoReader reader = new ProdutoReader();
-		reader.ler(path);
+		reader.cargaInicial(path);
 		//throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
 	}
 
