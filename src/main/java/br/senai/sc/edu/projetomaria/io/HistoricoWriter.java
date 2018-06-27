52803ee73c5df4606c5f1a2815f214e7f9eb6768
Mauro <maurodsjunior@gmail.com>
2018-06-27 20:50:47 -0300

    Alex/Mauro

--- src/main/java/br/senai/sc/edu/projetomaria/io/HistoricoWriter.java
+++ src/main/java/br/senai/sc/edu/projetomaria/io/HistoricoWriter.java
@@ -1,4 +1,5 @@
 package br.senai.sc.edu.projetomaria.io;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -16,48 +17,37 @@ public class HistoricoWriter {
 
 	private static final String separadorLinhas = "\n";
 	private static final Logger LOGGER = LogManager.getLogger();
-	private  static final Object[] colunasArquivo = { "id_historico", "mes_ano", "quantidade", "produto_sku", "id_canal" };
+	private static final Object[] colunasArquivo = { "id_historico", "mes_ano", "quantidade", "produto_sku",
+			"id_canal" };
 
-	public void writeCsvFile(Path nomeArquivo, List <Historico> registro) {
-		
-		FileWriter escritorDeArquivos = null;
+	public void writeCsvFile(Path nomeArquivo, List<Historico> registro) {
 
 		CSVPrinter csvCompiladorDeArquivos = null;
 
 		CSVFormat formatacaoCsv = CSVFormat.DEFAULT.withRecordSeparator(separadorLinhas).withDelimiter(';');
 
-		try {
-			escritorDeArquivos = new FileWriter(nomeArquivo.toFile());
+		try (FileWriter escritorDeArquivos = new FileWriter(nomeArquivo.toFile())) {
 
 			csvCompiladorDeArquivos = new CSVPrinter(escritorDeArquivos, formatacaoCsv);
 
 			csvCompiladorDeArquivos.printRecord(colunasArquivo);
-				
+
 			for (Historico historico : registro) {
-//				historico.getId();
-//				historico.getPeriodo();
-//				historico.getQuantidade();
-//				historico.getProduto().getSku();
-//				historico.getCanal();
-				csvCompiladorDeArquivos.printRecord(historico.getId(), historico.getPeriodo(), historico.getQuantidade(), historico.getProduto().getSku(), historico.getCanal());
+				// historico.getId();
+				// historico.getPeriodo();
+				// historico.getQuantidade();
+				// historico.getProduto().getSku();
+				// historico.getCanal();
+				csvCompiladorDeArquivos.printRecord(historico.getId(), historico.getPeriodo(),
+						historico.getQuantidade(), historico.getProduto().getSku(), historico.getCanal());
 			}
-			
-			
+
 			LOGGER.info(Messages.ARQUIVO_CRIADO_COM_SUCESSO);
 
-		} catch (Exception e) {
-			
+		} catch (Exception expc) {
+
 			LOGGER.info(Messages.ERRO_ESCRITOR_DE_ARQUIVO);
-			e.printStackTrace();
-		} finally {
-			try {
-				escritorDeArquivos.flush();
-				escritorDeArquivos.close();
-				csvCompiladorDeArquivos.close();
-			} catch (IOException e) {		
-				LOGGER.info(Messages.ERRO_AO_ENVIAR);
-				e.printStackTrace();
-			}
+			expc.printStackTrace();
 		}
 	}
 }
