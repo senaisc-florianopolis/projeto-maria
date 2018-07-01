65d3c3e4cb7c4152bac0bb084945882e64293bdf
Gabriel Segalla <grsegalla@hotmail.com>
2018-07-01 19:04:57 -0300

    adjustments try with resources

--- src/main/java/br/senai/sc/edu/projetomaria/resource/SQL.java
+++ src/main/java/br/senai/sc/edu/projetomaria/resource/SQL.java
@@ -3,11 +3,14 @@ package br.senai.sc.edu.projetomaria.resource;
 public abstract class SQL {
 
 	// ....
-	public static String INSERT_CANAL ;
+	public static final String GET_CANAL ;
+	
+	public static final String INSERT_CANAL ;
 
 	public static final String UPDATE_CANAL ;
 
 	public static final String DELETE_CANAL;
+	
 	public static final String HISTORICO_SELECT;
 
 	public static final String HISTORICO_INSERT;
@@ -28,6 +31,7 @@ public abstract class SQL {
 		HISTORICO_INSERT = ResourceManager.getSQL("historico.insert"); //$NON-NLS-1$
 		HISTORICO_UPDATE = ResourceManager.getSQL("historico.update"); //$NON-NLS-1$
 		HISTORICO_DELETE = ResourceManager.getSQL("historico.delete"); //$NON-NLS-1$
+		GET_CANAL = ResourceManager.getSQL("sql.get.todos.canal"); //$NON-NLS-1$
 		INSERT_CANAL = ResourceManager.getSQL ("sql.insert.canal"); //$NON-NLS-1$
 		UPDATE_CANAL = ResourceManager.getSQL ("sql.update.canal"); //$NON-NLS-1$
 		DELETE_CANAL = ResourceManager.getSQL("sql.delete.canal"); //$NON-NLS-1$
