217cfb8a7457d8d0983fd540715ce327e1e0a3bb
danielP <39319339+danielPrando@users.noreply.github.com>
2018-07-03 16:25:08 -0300

    ajuste relatorio de estatistica

--- src/main/java/br/senai/sc/edu/projetomaria/io/EstimativaWritter.java
+++ src/main/java/br/senai/sc/edu/projetomaria/io/EstimativaWritter.java
@@ -25,13 +25,20 @@ public class EstimativaWritter {
 			Estimativa estimativa = new Estimativa();
 			String periodoEstimativa = "";
 
-			for (Resultado periodoUti : estimativa.Calculo(periodo)) {
-				for (Integer i : periodoUti.getPeriodo_utilizado()) {
-					periodoEstimativa += i + ",";
-				}
-			}
-
 			for (Resultado linha : estimativa.Calculo(periodo)) {
+				periodoEstimativa = "";
+				int tamanho = linha.getPeriodo_utilizado().length;
+				int cont = 0;
+
+				for (Integer i : linha.getPeriodo_utilizado()) {
+					cont++;
+					if (cont < tamanho) {
+						periodoEstimativa += i + ",";
+					}
+					if (cont == tamanho) {
+						periodoEstimativa += i;
+					}
+				}
 				escrever.printRecord(linha.getSKU(), linha.getPeriodo_total(), linha.getResultado_media(),
 						linha.getResultado_media2(), linha.getResultado_media3(), linha.getResultado_media4(),
 						linha.getErro_quadratico_medio(), periodoEstimativa);
