5fcbe6a9b8b03d3a61ceff0a15563caa64712a31
CST ADS <CST ADS@SN-351164.lan>
2018-10-09 21:52:29 -0300

    ADD COMENTS

--- src/test/java/HU4CargaFamiliaTest.java
+++ src/test/java/HU4CargaFamiliaTest.java
@@ -4,8 +4,15 @@ import org.junit.jupiter.api.Test;
 
 class HU4CargaFamiliaTest {
 
+	/**
+	 * Teste de historia de usario bdd 3
+	 * 
+	 * @author Yuri Mansani <yurimv9@gmail.com>
+	 * 
+	 *  
+	 */
 	@Test
-	void test() {
+	void bdd3() {
 		fail("Not yet implemented");
 	}
 
