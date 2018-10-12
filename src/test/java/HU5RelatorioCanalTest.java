240753934a585e06609bcaeeffc060143f0a3067
Aluno <vinicius_paniz@estudante.sc.senai.br>
2018-10-11 21:13:32 -0300

    aula11/10/2018

--- src/test/java/HU5RelatorioCanalTest.java
+++ src/test/java/HU5RelatorioCanalTest.java
@@ -1,12 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
 class HU5RelatorioCanalTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void nomeArquivo() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		try {
+			Path p = Paths.get(classLoader.getResource("dataset/relatorio_canal.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
