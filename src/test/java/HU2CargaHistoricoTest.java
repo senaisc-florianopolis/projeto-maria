3d00d716cd92e26bac1b9e3ad9eeaa1c4f163a64
CST ADS <CST ADS@10.3.61.156>
2018-10-11 21:44:48 -0300

    inserts.csv

--- src/test/java/HU2CargaHistoricoTest.java
+++ src/test/java/HU2CargaHistoricoTest.java
@@ -1,12 +1,89 @@
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import br.senai.sc.edu.projetomaria.service.CargaService;
+
 class HU2CargaHistoricoTest {
+	CargaService service = null;
+		
+	@BeforeAll
+	void before() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		Path cf = null;
+		Path cp = null;
+		Path cc = null;
+		Path ch = null;
+		
+		try {
+			cf = Paths.get(classLoader.getResource("dataset/carga-familia-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			cp = Paths.get(classLoader.getResource("dataset/carga-produto-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			cc = Paths.get(classLoader.getResource("dataset/carga-canal-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			ch = Paths.get(classLoader.getResource("dataset/carga-historico-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// ------------------------------------Services------------------------------ //
+		
+		service = new CargaService();
+		try {
+			service.insertFamilia(cf);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		service.insertProduto(cp);
+		try {
+			service.insertCanal(cc);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		service.insertHistorico(ch);
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void caso1sucesso(Path path) {
+		service.deleteHistorico(path);
+		// usar os asserts pra ver se está tudo correto;
+
+	}
+	void caso1erro1(Path path) {
+		service.deleteHistorico(path);
+		// usar os asserts pra ver se está tudo correto;
+
+	}
+	void caso1erro2(Path path) {
+		service.deleteHistorico(path);
+		// usar os asserts pra ver se está tudo correto;
+
+	}
+
+	@AfterAll
+	void after() {
+		
+
 	}
 
 }
