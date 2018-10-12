94087c50640ac85d8217dd818ae49d824973eda6
Aluno <vinicius_paniz@estudante.sc.senai.br>
2018-10-11 22:03:46 -0300

    aula11/2018parte2

--- src/test/java/HU5RelatorioCanalTest.java
+++ src/test/java/HU5RelatorioCanalTest.java
@@ -1,22 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import br.senai.sc.edu.projetomaria.service.RelatorioService;
 
 class HU5RelatorioCanalTest {
-
-	@Test
-	void nomeArquivo() {
-		ClassLoader classLoader = getClass().getClassLoader();
+	static RelatorioService service = null;
+	
+	@BeforeAll
+	
+	static	void before() {
+		ClassLoader classLoader = HU5RelatorioCanalTest.class.getClassLoader();
+		Path p = null;
 		try {
-			Path p = Paths.get(classLoader.getResource("dataset/relatorio_canal.csv").toURI());
+			 p = Paths.get(classLoader.getResource("dataset/relatorio_canal.csv").toURI());
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		service = new RelatorioService();
+		service.insertProduto(p);
+		service.insertCanal(p);
 	}
 
 }
