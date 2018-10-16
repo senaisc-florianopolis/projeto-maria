a41715c0c20626cade3b222140ae15fd4cd7af00
Aluno <vinicius_paniz@estudante.sc.senai.br>
2018-10-16 20:59:25 -0300

    aula16/10/2018

--- src/test/java/HU5RelatorioCanalTest.java
+++ src/test/java/HU5RelatorioCanalTest.java
@@ -1,28 +1,53 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import br.senai.sc.edu.projetomaria.service.RelatorioService;
 
 class HU5RelatorioCanalTest {
 	static RelatorioService service = null;
 	
-	@BeforeAll
-	
-	static	void before() {
+	@BeforeAll	
+	static void beforeAll() {
 		ClassLoader classLoader = HU5RelatorioCanalTest.class.getClassLoader();
-		Path p = null;
+		Path path = null;
 		try {
-			 p = Paths.get(classLoader.getResource("dataset/relatorio_canal.csv").toURI());
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
+			path = Paths.get(classLoader.getResource("dataset/relatorio_canal.csv").toURI());
+		} catch (URISyntaxException e) {			
 			e.printStackTrace();
 		}
-		service = new RelatorioService();
-		service.insertProduto(p);
-		service.insertCanal(p);
-	}
+		service = new RelatorioService(); 
+		try {
+			service.exportRelatorioCanal(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("falha");
+		}
 
+	}
+	
+	@Test	
+	void RelatorioService() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		Path path = null;
+		try {
+			 path = Paths.get(classLoader.getResource("dataset/relatorio_canal.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		try {
+			service.exportRelatorioCanal(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("falha");
+		}
+	}
 }
+	
+
