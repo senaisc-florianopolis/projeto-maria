1d881e68ad051891f7c6425ed173234985f63fac
danielPrandi <daniel_prando@estudante.sc.senai.br>
2018-10-23 20:23:07 -0300

    Test

---
+++ src/test/java/HU1CargaCanalTestS1.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class HU1CargaCanalTestS1 {
+
+	@Test
+	void test() {
+		fail("Erro! id inexistente.");
+	}
+
+}
