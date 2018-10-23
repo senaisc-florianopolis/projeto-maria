5f3e2d7fe86ac1b83fde5afffb409b5447834a42
CST ADS <CST ADS@10.3.61.107>
2018-10-23 19:27:52 -0300

    Teste

--- src/test/java/HU3CargaProdutoTest.java
+++ src/test/java/HU3CargaProdutoTest.java
@@ -1,12 +1,62 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+
+import javax.sound.midi.Patch;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class HU3CargaProdutoTest {
+import br.senai.sc.edu.projetomaria.service.CargaService;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class HU3CargaProdutoTest {
+	static CargaService service = null;
+
+	@BeforeAll
+	static void before() {
+		service = new CargaService();
+		ClassLoader classLoader = HU3CargaProdutoTest.class.getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto_insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		CargaService.insertProduto(p);
+
 	}
 
+	@Test
+	void TesteSucesso() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto_insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		CargaService.insertProduto(p);
+
+	
+	}
+
+	@AfterAll
+	static  void elimina() {
+		ClassLoader classLoader = HU3CargaProdutoTest.class.getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto_insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		service.deleteProduto(p);
+		System.out.println("O arquivo excluido com sucesso!");
+	}
 }
