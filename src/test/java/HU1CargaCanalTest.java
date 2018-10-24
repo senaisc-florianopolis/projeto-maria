37dcaa46a907ad1f17b46b4a515b7e94a057d4cc
Thiago Garcia <thiago_sus@estudante.sc.senai.br>
2018-10-23 21:34:32 -0300

    Linha em branco

--- src/test/java/HU1CargaCanalTest.java
+++ src/test/java/HU1CargaCanalTest.java
@@ -1,12 +1,89 @@
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.fail;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
 
-class HU1CargaCanalTest {
+import br.senai.sc.edu.projetomaria.service.CargaService;
 
+class HU1CargaCanalTest {
+	static CargaService service = null;
+
+	// HU1 - BDD 4 - Não Duplicação (Teste de sucesso)
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void InserirNaoDuplicado() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/hu1bdd4carga_canal_insert.csv").toURI());
+		} catch (URISyntaxException e) {
+
+			e.printStackTrace();
+		}
+		try {
+			service.insertCanal(p);
+			service.insertCanal(p);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			fail("Alteração Realizada com sucesso!");
+		}
+	}
+	
+
+	// HU1 - BDD 6 – Erro de Update – Id não existe (Teste de Sucesso)
+	@Test
+	void ErroUpdate() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/hu1bdd6carga_canal_update.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		try {
+			service.updateCanal(p);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			fail("Impossivel fazer update, ID não existente!");
+		}
 	}
 
+	// HU1 - BDD 6 – Erro de Delete – Id não existe (Teste de Falha 1)
+
+	@Test
+	void insertCanal() {
+
+		ClassLoader classLoader = HU1CargaCanalTest.class.getClassLoader();
+		Path i = null;
+		try {
+			i = Paths.get(classLoader.getResource("dataset/hu1bdd6carga_canal_insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service = new CargaService();
+		try {
+			service.insertCanal(i);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		///////////////////////////
+
+		Path j = null;
+		try {
+			j = Paths.get(classLoader.getResource("dataset/hu1bdd6carga_canal_delete.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		try {
+			service.deleteCanal(j);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
