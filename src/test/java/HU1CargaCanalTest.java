c61c4970544b876ffd210c8d3e6d7d25eb6d5d60
Antonio Moura <antoniomouradev@gmail.com>
2018-10-25 20:57:33 -0300

    testes

--- src/test/java/HU1CargaCanalTest.java
+++ src/test/java/HU1CargaCanalTest.java
@@ -1,12 +1,31 @@
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
+
+import br.senai.sc.edu.projetomaria.service.CargaService;
 
 class HU1CargaCanalTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void teste01() throws SQLException {
 
+		ClassLoader classLoader = getClass().getClassLoader();
+		Path p = null;
+
+		try {
+			CargaService servicoCarga = new CargaService();
+			p = Paths.get(classLoader.getResource("dataset/teste.csv").toURI());
+
+			servicoCarga.insertFamilia(p);
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+
+		}
+
+	}
 }
--- src/test/java/HU8RelatorioEstimativaTest.java
+++ src/test/java/HU8RelatorioEstimativaTest.java
@@ -1,12 +1,65 @@
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class HU8RelatorioEstimativaTest {
+import br.senai.sc.edu.projetomaria.service.CargaService;
+import br.senai.sc.edu.projetomaria.service.RelatorioService;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class teste01 {
+
+	static CargaService cargaService;
+
+	@BeforeAll
+	static void cargaTabelas() {
+		ClassLoader classLoader = teste01.class.getClassLoader();
+		cargaService = new CargaService();
+
+		Path f = null;
+		try {
+			f = Paths.get(classLoader.getResource("dataset/carga-familia-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		try {
+			cargaService.insertFamilia(f);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga-produto-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		cargaService.insertProduto(p);
 	}
 
+	@Test
+	void teste02() {
+		RelatorioService relatorioService = new RelatorioService();
+		try {
+			Path tempFile = Files.createTempFile("relatorio_familia_comercial", ".txt");
+			relatorioService.exportRelatorioProduto(tempFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			Path tempFile = Files.createTempFile("relatorio_produto", ".txt");
+			relatorioService.exportRelatorioProduto(tempFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
