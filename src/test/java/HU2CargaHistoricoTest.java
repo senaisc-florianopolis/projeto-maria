644115a4743b77307db90d8014f34047d284d149
Luciano Antonio Costa <16106846+la-costa@users.noreply.github.com>
2018-10-27 15:28:17 -0300

    Revert "Teste unitários  (#17)"
    This reverts commit 702db445fd35c60c05ce3245b06a66c65005f770.

--- src/test/java/HU2CargaHistoricoTest.java
+++ src/test/java/HU2CargaHistoricoTest.java
@@ -1,85 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.SQLException;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import br.senai.sc.edu.projetomaria.service.CargaService;
-import br.senai.sc.edu.projetomaria.service.RelatorioService;
 
 class HU2CargaHistoricoTest {
 
-	static CargaService cargaService;
-
-	@BeforeAll
-	static void cargaTabelas() throws SQLException {
-		ClassLoader classLoader = HU7RelatorioProdutoTest.class.getClassLoader();
-		cargaService = new CargaService();
-
-		Path f = null;
-		try {
-			f = Paths.get(classLoader.getResource("dataset/carga_historico_insert.csv").toURI());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		cargaService.insertHistorico(f);
-
-		Path p = null;
-		try {
-			p = Paths.get(classLoader.getResource("dataset/carga_historico_insert.csv").toURI());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		cargaService.insertHistorico(p);
-	}
-
 	@Test
-	void relatorioHistoricoSucesso() {
-		RelatorioService relatorioService = new RelatorioService();
-		try {
-			Path tempFile = Files.createTempFile("relatorio_historico_comercial", ".txt");
-			relatorioService.exportRelatorioHistorico(tempFile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		try {
-			Path tempFile = Files.createTempFile("relatorio_historico", ".txt");
-			relatorioService.exportRelatorioHistorico(tempFile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	@AfterAll
-	static void deletaTabela() {
-		ClassLoader classLoader = HU2CargaHistoricoTest.class.getClassLoader();
-
-		Path f = null;
-		try {
-			f = Paths.get(classLoader.getResource("dataset/carga_historico_delete.csv").toURI());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		cargaService.deleteHistorico(f);
-
-		Path p = null;
-		try {
-			p = Paths.get(classLoader.getResource("dataset/carga_historico_delete.csv").toURI());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		cargaService.deleteHistorico(p);
+	void test() {
+		fail("Not yet implemented");
 	}
 
 }
