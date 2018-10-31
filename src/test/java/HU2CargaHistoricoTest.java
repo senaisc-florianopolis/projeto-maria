75e02efa64e30025a10e5d583ee4287c69747fd1
Alexandre Barreto de Souza <alexandrebdesouza1@gmail.com>
2018-10-31 19:58:12 -0300

    Ajustes de erros  HU2 , HU3

--- src/test/java/HU2CargaHistoricoTest.java
+++ src/test/java/HU2CargaHistoricoTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -20,7 +20,7 @@ class HU2CargaHistoricoTest {
 
 	@BeforeAll
 	static void cargaTabelas() throws SQLException {
-		ClassLoader classLoader = HU7RelatorioProdutoTest.class.getClassLoader();
+		ClassLoader classLoader = HU2CargaHistoricoTest.class.getClassLoader();
 		cargaService = new CargaService();
 
 		Path f = null;
@@ -81,5 +81,154 @@ class HU2CargaHistoricoTest {
 		}
 		cargaService.deleteHistorico(p);
 	}
+	
+	
+	static CargaService service = null;
+	
+	static ClassLoader classLoader = HU2CargaHistoricoTest.class.getClassLoader();
+	
+	@BeforeAll
+	static void before() {
+		
+		service = new CargaService();
+		
+		Path c = null;
+		Path f = null;
+		Path p = null;
+		
+		
+		try {
+			c = Paths.get(classLoader.getResource("dataset/carga_canal_insert_vitorhu2.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			service.insertCanal(c);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			f = Paths.get(classLoader.getResource("dataset/carga_familia_insert_vitorhu2.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			service.insertFamilia(f);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto_insert_vitorhu2.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service.insertProduto(p);
+	}
+	
+	@Test
+	void testBDD3Sucesso(){
+		
+		System.out.println("------------------TESTE BDD3 SUCESSO------------------");
+		
+		service = new CargaService();
+		
+		Path insert = null;
+		try {
+			insert = Paths.get(classLoader.getResource("dataset/carga_historico_insert_vitorhu2.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service.insertHistorico(insert);
+		
+		
+		Path delete = null;
+		try {
+			delete = Paths.get(classLoader.getResource("dataset/carga_historico_delete_vitorhu2.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service.deleteHistorico(delete); 
+		
+		System.out.println("------------------TESTE BDD3 SUCESSO------------------");
+	}
+	
+	@Test
+	void testBDD3Erro1(){
+		
+		System.out.println("------------------TESTE BDD 3 ERRO1------------------");
+		
+		service = new CargaService();
+		
+		Path insert = null;
+		try {
+			insert = Paths.get(classLoader.getResource("dataset/carga_historico_insert_idrepetido.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service.insertHistorico(insert);
+		
+		assertThrows(SQLException.class, () -> {
+			Path delete = null;
+			try {
+				delete = Paths.get(classLoader.getResource("dataset/carga_historico_delete_idrepetido.csv").toURI());
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+			service.deleteHistorico(delete);
+			System.out.println("------------------TESTE BDD3 ERRO1------------------");
+		});
+	}
+	
+	@Test
+	void testBDD3Erro2(){
+		
+		System.out.println("------------------TESTE BDD3 ERRO2------------------");
+		
+		service = new CargaService();
+		
+		Path insert = null;
+		try {
+			insert = Paths.get(classLoader.getResource("dataset/carga_historico_insert_nada.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service.insertHistorico(insert);
+		
+		assertThrows(SQLException.class, () -> {
+			Path delete = null;
+			try {
+				delete = Paths.get(classLoader.getResource("dataset/carga_historico_delete_vitorhu2.csv").toURI());
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+			service.deleteHistorico(delete);
+			System.out.println("------------------TESTE BDD3 ERRO2------------------");
+		});
 
-}
+	}
+
+	@Test
+	void testBDD9Sucesso() {
+		
+		System.out.println("------------------TESTE BDD9 SUCESSO------------------");
+		
+		service = new CargaService();
+		
+		Path insert = null;
+		try {
+			insert = Paths.get(classLoader.getResource("dataset/carga_historico_insert_branco.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service.insertHistorico(insert);
+		System.out.println("------------------TESTE BDD9 SUCESSO------------------");
+	
+	}
+	}
+	
--- src/test/java/HU3CargaProdutoTest.java
+++ src/test/java/HU3CargaProdutoTest.java
@@ -1,11 +1,17 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
+
+
+import javax.sound.midi.Patch;
+
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,81 +21,132 @@ import br.senai.sc.edu.projetomaria.service.CargaService;
 import br.senai.sc.edu.projetomaria.service.RelatorioService;
 
 class HU3CargaProdutoTest {
-	 CargaService cargaService;
+	CargaService cargaService;
 
-			@BeforeAll
-			void cargaTabelas() {
-				ClassLoader classLoader = HU7RelatorioProdutoTest.class.getClassLoader();
-				cargaService = new CargaService();
+	@BeforeAll
+	void cargaTabelas() {
+		ClassLoader classLoader = HU3CargaProdutoTest.class.getClassLoader();
+		cargaService = new CargaService();
 
-				Path f = null;
-				try {
-					f = Paths.get(classLoader.getResource("dataset/carga-familia-insert.csv").toURI());
-				} catch (URISyntaxException e) {
-					e.printStackTrace();
-				}
-				try {
-					cargaService.insertFamilia(f);
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
+		Path f = null;
+		try {
+			f = Paths.get(classLoader.getResource("dataset/carga-familia-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		try {
+			cargaService.insertFamilia(f);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 
-				Path p = null;
-				try {
-					p = Paths.get(classLoader.getResource("dataset/carga-produto-insert.csv").toURI());
-				} catch (URISyntaxException e) {
-					e.printStackTrace();
-				}
-				cargaService.insertProduto(p);
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga-produto-insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		cargaService.insertProduto(p);
+	}
+
+	@Test
+	void relatorioProdutoSucesso() {
+		RelatorioService relatorioService = new RelatorioService();
+		try {
+			Path tempFile = Files.createTempFile("relatorio_familia_comercial", ".txt");
+			relatorioService.exportRelatorioProduto(tempFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			Path tempFile = Files.createTempFile("relatorio_produto", ".txt");
+			relatorioService.exportRelatorioProduto(tempFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@AfterAll
+	void deletaTabela() {
+		ClassLoader classLoader = HU7RelatorioProdutoTest.class.getClassLoader();
+		cargaService = new CargaService();
+
+		Path f = null;
+		try {
+			f = Paths.get(classLoader.getResource("dataset/carga_familia_delete.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		try {
+			cargaService.deleteFamilia(f);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto_delete.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		cargaService.deleteProduto(p);
+	}
+
+	static CargaService service = null;
+
+	@BeforeAll
+	static void before() {
+		service = new CargaService();
+		ClassLoader classLoader = HU3CargaProdutoTest.class.getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto_insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		service.insertProduto(p);
+
+	}
+
+	@Test
+	void Teste() {
+
+		service = new CargaService();
+		ClassLoader classLoader = HU3CargaProdutoTest.class.getClassLoader();
+			Path p = null;
+			try {
+				p = Paths.get(classLoader.getResource("dataset/carga_produto_insert.csv").toURI());
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				service.deleteProduto(p);
+				System.out.println("O arquivo excluido com sucesso!");
 			}
+		
 
-			@Test
-			void relatorioProdutoSucesso() {
-				RelatorioService relatorioService = new RelatorioService();
-				try {
-					Path tempFile = Files.createTempFile("relatorio_familia_comercial", ".txt");
-					relatorioService.exportRelatorioProduto(tempFile);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+	}
 
-				try {
-					Path tempFile = Files.createTempFile("relatorio_produto", ".txt");
-					relatorioService.exportRelatorioProduto(tempFile);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 
-			}
+//
+	@AfterAll
+	static void elimina() {
+		service = new CargaService();
+		ClassLoader classLoader = HU3CargaProdutoTest.class.getClassLoader();
 
-			@AfterAll
-			void deletaTabela() {
-				ClassLoader classLoader = HU7RelatorioProdutoTest.class.getClassLoader();
-				cargaService = new CargaService();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto_insert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		service.deleteProduto(p);
+		System.out.println("O arquivo excluido com sucesso!");
 
-				Path f = null;
-				try {
-					f = Paths.get(classLoader.getResource("dataset/carga_familia_delete.csv").toURI());
-				} catch (URISyntaxException e) {
-					e.printStackTrace();
-				}
-				try {
-					cargaService.deleteFamilia(f);
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-
-				Path p = null;
-				try {
-					p = Paths.get(classLoader.getResource("dataset/carga_produto_delete.csv").toURI());
-				} catch (URISyntaxException e) {
-					e.printStackTrace();
-				}
-				cargaService.deleteProduto(p);
-			}
-
-			
-
+	}
 }
