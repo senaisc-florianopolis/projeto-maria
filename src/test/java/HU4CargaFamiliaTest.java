71f562cd81963aeecaa0f5026d66897a767ae0db
Leonardo Kovaliski <leonado_kovaliski@estudante.sc.senai.br>
2018-10-31 20:12:42 -0200

    atualização de imports

--- src/test/java/HU4CargaFamiliaTest.java
+++ src/test/java/HU4CargaFamiliaTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.fail;
 
 import java.net.URISyntaxException;
 import java.nio.file.Path;
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.senai.sc.edu.projetomaria.service.CargaService;
