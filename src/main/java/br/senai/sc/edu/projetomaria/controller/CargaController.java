e9741786e596211957d91d00b0072af8edc37dbd
Luciano Antonio Costa <16106846+la-costa@users.noreply.github.com>
2018-11-02 14:28:35 -0300

    Refactoring iniciado

--- src/main/java/br/senai/sc/edu/projetomaria/controller/CargaController.java
+++ src/main/java/br/senai/sc/edu/projetomaria/controller/CargaController.java
@@ -1,88 +1,35 @@
 package br.senai.sc.edu.projetomaria.controller;
 
-import java.sql.SQLException;
-
 import br.senai.sc.edu.projetomaria.cli.CommandCarga;
 import br.senai.sc.edu.projetomaria.resource.Messages;
 import br.senai.sc.edu.projetomaria.service.CargaService;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse;
 
 public class CargaController {
 
-	public void exec(CommandCarga command) throws SQLException {
+	public ServiceResponse exec(CommandCarga command) {
+		CargaService service = new CargaService();
+		ServiceResponse response = null;
 		switch (command.getTipo()) {
 		case CANAL:
-			this.execCanal(command);
+			response = service.cargaCanal(command.getArquivo());
 			break;
 		case FAMILIA:
-			this.execFamilia(command);
+			response = service.cargaFamilia(command.getArquivo());
 			break;
 		case PRODUTO:
-			this.execProduto(command);
+			response = service.cargaProduto(command.getArquivo());
 			break;
 		case HISTORICO:
-			this.execHistorico(command);
+			response = service.cargaHistorico(command.getArquivo());
 			break;
 		case PHASE:
-			this.execPhase(command);
+			response = service.cargaPhase(command.getArquivo());
 			break;
 		default:
 			throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
 		}
-	}
-	
-	protected void execPhase(CommandCarga command) {
-		CargaService service = new CargaService();
-		if (command.isInsert()) {
-			service.insertPhase(command.getArquivo());
-		} else if (command.isDelete()) {
-			service.deletePhase(command.getArquivo());
-		} else if (command.isUpdate()) {
-			service.updatePhase(command.getArquivo());
-		}
-	}
-
-	protected void execCanal(CommandCarga command) throws SQLException {
-		CargaService service = new CargaService();
-		if (command.isInsert()) {
-			service.insertCanal(command.getArquivo());
-		} else if (command.isDelete()) {
-			service.deleteCanal(command.getArquivo());
-		} else if (command.isUpdate()) {
-			service.updateCanal(command.getArquivo());
-		}
-	}
-
-	protected void execFamilia(CommandCarga command) throws SQLException {
-		CargaService service = new CargaService();
-		if (command.isInsert()) {
-			service.insertFamilia(command.getArquivo());
-		} else if (command.isDelete()) {
-			service.deleteFamilia(command.getArquivo());
-		} else if (command.isUpdate()) {
-			service.updateFamilia(command.getArquivo());
-		}
-	}
-
-	protected void execProduto(CommandCarga command) {
-		CargaService service = new CargaService();
-		if (command.isInsert()) {
-			service.insertProduto(command.getArquivo());
-		} else if (command.isDelete()) {
-			service.deleteProduto(command.getArquivo());
-		} else if (command.isUpdate()) {
-			service.updateProduto(command.getArquivo());
-		}
-	}
-
-	protected void execHistorico(CommandCarga cm) {
-		CargaService service = new CargaService();
-		if (cm.isInsert()) {
-			service.insertHistorico(cm.getArquivo());
-		} else if (cm.isDelete()) {
-			service.deleteHistorico(cm.getArquivo());
-		} else if (cm.isUpdate()) {
-			service.updateHistorico(cm.getArquivo());
-		}
+		return response;
 	}
 
 }
