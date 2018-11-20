0920bc66a3332b06483893cba94600cd29a2d961
André Pizzetti <pessetti@gmail.com>
2018-11-20 21:29:51 -0200

    UPSERT - v 1.0

--- src/main/java/br/senai/sc/edu/projetomaria/dao/HistoricoDAO.java
+++ src/main/java/br/senai/sc/edu/projetomaria/dao/HistoricoDAO.java
@@ -45,33 +45,46 @@ public class HistoricoDAO extends AbstractDAO {
 
 		return registro;
 	}
-
-	public void persist(List<Historico> registro) {
-
-		String sql = SQL.HISTORICO_INSERT;
-
-
-		try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
-			for (Historico historico : registro) {
+	
+	public void upsert (List<Historico> registro) {
+		String sql = "";
+		int successes = 0;
+		int total = 0;			
+		
+		for (Historico historico : registro) {
+		sql = "INSERT INTO historico (MES_ANO,PRODUTO_SKU,ID_CANAL,QUANTIDADE) VALUES (?,?,?,?)"+
+		"ON DUPLICATE KEY UPDATE MES_ANO=?,PRODUTO_SKU=?,ID_CANAL=?,QUANTIDADE= ?";	
+		try (Connection conn = getConnection();
+				PreparedStatement ps = conn.prepareStatement(sql);) {
 				LOGGER.debug(historico);
-				ps.setDate(1, java.sql.Date.valueOf(historico.getPeriodo()));
-				ps.setInt(2, historico.getQuantidade());
-				ps.setInt(3, historico.getProduto().getSku());
-				ps.setInt(4, historico.getCanal().getId());
+				ps.setDate(1,java.sql.Date.valueOf(historico.getPeriodo()));
+				ps.setInt(2,historico.getQuantidade());
+				ps.setInt(3,historico.getProduto().getSku());
+				ps.setInt(4,historico.getCanal().getId());
+				ps.setInt(5,historico.getId());
 				LOGGER.debug(ps);
-				ps.execute();
+				ps.executeUpdate(sql);
+				successes++;
+			} catch (SQLException e) {
+				if(e.getErrorCode() == 1062) {
+					LOGGER.info("Há registros duplicados. Retire-os e tente novamente. Mensagem SQL = " + e.getMessage());
+					//Realmente existe a necessidade? Se já tem vai fazer update.
+				} else if (e.getErrorCode() == 1) {
+					LOGGER.info("Existe uma linha em branco. Ajuste e tente novamente. Mensagem SQL = " + e.getMessage());
+				} else if (e.getErrorCode() == 2) {
+					LOGGER.info("Existe uma coluna em branco. Ajuste e tente novamente. Mensagem SQL = " + e.getMessage());
+				} else {
+					LOGGER.info("Registro fora do Padrão. Retire-os e tente novamente. Mensagem SQL = " + e.getMessage());
+				}
 			}
-		} catch (SQLException e) {
-			if(e.getErrorCode() == 1062) {
-				LOGGER.info("Há registros duplicados. Retire-os e tente novamente. Mensagem SQL = " + e.getMessage());
-			}else {
-				LOGGER.error(e);
-			}
-		}
+		total++;
+	}
+		LOGGER.info(successes + " de " + total + " " + Messages.SUCCESS_PRODUTO);
 	}
 
 
-	public void update(List<Historico> registro) {
+	/*
+	 public void update(List<Historico> registro) {
 
 		String sql = SQL.HISTORICO_UPDATE;
 
@@ -95,6 +108,31 @@ public class HistoricoDAO extends AbstractDAO {
 			LOGGER.error(e);
 		}
 	}
+	
+	public void persist(List<Historico> registro) {
+
+		String sql = SQL.HISTORICO_INSERT;
+
+
+		try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+			for (Historico historico : registro) {
+				LOGGER.debug(historico);
+				ps.setDate(1, java.sql.Date.valueOf(historico.getPeriodo()));
+				ps.setInt(2, historico.getQuantidade());
+				ps.setInt(3, historico.getProduto().getSku());
+				ps.setInt(4, historico.getCanal().getId());
+				LOGGER.debug(ps);
+				ps.execute();
+			}
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 1062) {
+				LOGGER.info("Há registros duplicados. Retire-os e tente novamente. Mensagem SQL = " + e.getMessage());
+			}else {
+				LOGGER.error(e);
+			}
+		}
+	}
+	*/
 
 	public void delete(List<Historico> registro) {
 
