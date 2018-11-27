6cdedf9aed6edc7866d362898cbdf9ece5a6af30
rondonVitor <rondon.vitor@yahoo.com.br>
2018-11-27 21:34:49 -0200

    começando produtoService

--- src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
+++ src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
@@ -1,10 +1,14 @@
 package br.senai.sc.edu.projetomaria.service;
 
 import java.nio.file.Path;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import br.senai.sc.edu.projetomaria.dao.ProdutoDAO;
+import br.senai.sc.edu.projetomaria.io.ProdutoReader;
+import br.senai.sc.edu.projetomaria.model.Produto;
 import br.senai.sc.edu.projetomaria.resource.Messages;
 
 public class CargaService {
@@ -19,6 +23,11 @@ public class CargaService {
 	}
 
 	public ServiceResponse cargaProduto(Path path) {
+		ProdutoReader produtoReader = new ProdutoReader();
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		
+		produtoDAO.upsert(produtoReader.lerCsvProduto(path));
+		
 		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
 	}
 
