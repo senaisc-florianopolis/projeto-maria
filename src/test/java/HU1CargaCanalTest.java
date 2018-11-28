1dd8afbf21c029ff8de0506b9e28277a687f9572
Robson <robccorreia@gmail.com>
2018-11-28 21:37:50 -0200

    ...

--- src/test/java/HU1CargaCanalTest.java
+++ src/test/java/HU1CargaCanalTest.java
@@ -28,36 +28,3 @@ class HU1CargaCanalTest {
 		assertEquals(s.getStatus(), STATUS.OK);
 	}
 }
-
-//import static org.junit.jupiter.api.Assertions.fail;
-//
-//import java.net.URISyntaxException;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.sql.SQLException;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import br.senai.sc.edu.projetomaria.service.CargaService;
-//
-//class HU1CargaCanalTest {
-// 	
-//	@Test		
-//	void testeInsert() throws SQLException {		
-//		ClassLoader classLoader	= getClass().getClassLoader();
-//		CargaService service = new CargaService();
-//		Path p = null;
-//		try {
-//			p = Paths.get(classLoader.getResource("dataset/carga_canal.csv").toURI());
-//		} catch (URISyntaxException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//			
-//		}
-//		service.cargaCanal(p);
-//		fail("falhou");
-//		
-//	}
-//	
-//	
-// }	
