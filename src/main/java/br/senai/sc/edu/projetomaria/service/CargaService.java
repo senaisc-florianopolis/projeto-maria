a2b7f0ae7cd8d4abd0f46c6b7831c42243ab2881
André Pizzetti <pessetti@gmail.com>
2018-11-28 20:55:10 -0200

    service

--- src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
+++ src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
@@ -1,11 +1,16 @@
 package br.senai.sc.edu.projetomaria.service;
 
 import java.nio.file.Path;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import br.senai.sc.edu.projetomaria.dao.HistoricoDAO;
+import br.senai.sc.edu.projetomaria.io.HistoricoReader;
+import br.senai.sc.edu.projetomaria.model.Historico;
 import br.senai.sc.edu.projetomaria.resource.Messages;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse.STATUS;
 
 public class CargaService {
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -27,7 +32,25 @@ public class CargaService {
 	}
 
 	public ServiceResponse cargaHistorico(Path path) {
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
+		//throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
+		HistoricoReader hist = new HistoricoReader();
+//	    hist.leitorDeArquivos(path);
+		
+		List<Historico> s = hist.leitorDeArquivos(path);
+		HistoricoDAO historicoDao = new HistoricoDAO();
+		
+		int[] result = historicoDao.upsert(s);
+		
+		
+		ServiceResponse response = new ServiceResponse(STATUS.OK, result)
+		
+		
+		
+		//ClassLoader classLoader = CargaService.class.getClassLoader();
+		//Path p = null;
+		//p = Paths.get(ClassLoader.getSystemResource("dataset/arquivo.csv").toURI());
+		//HistoricoDAO.class
+	
 	}
 
 }
