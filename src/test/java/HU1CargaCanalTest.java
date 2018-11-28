b071973bda8a7f9fa0289f480a372ac7f839b178
Robson <robccorreia@gmail.com>
2018-11-28 21:30:21 -0200

    reader e teste feito

--- src/test/java/HU1CargaCanalTest.java
+++ src/test/java/HU1CargaCanalTest.java
@@ -1,30 +1,63 @@
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
 
 import br.senai.sc.edu.projetomaria.service.CargaService;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse.STATUS;
 
 class HU1CargaCanalTest {
- 	
-	@Test		
-	void testeInsert() throws SQLException {		
-		ClassLoader classLoader	= getClass().getClassLoader();
-		CargaService service = new CargaService();
+
+	static CargaService service = null;
+	
+	@Test
+	static void testeInsert() {
+		ClassLoader classLoader = HU1CargaCanalTest.class.getClassLoader();
 		Path p = null;
 		try {
 			p = Paths.get(classLoader.getResource("dataset/carga_canal.csv").toURI());
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		}
-		service.cargaCanal(p);
-		fail("falhou");
-		
-	}		
- }	
+		service = new CargaService();
+		ServiceResponse s = service.cargaCanal(p);
+		assertEquals(s.getStatus(), STATUS.OK);
+	}
+}
+
+//import static org.junit.jupiter.api.Assertions.fail;
+//
+//import java.net.URISyntaxException;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.sql.SQLException;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import br.senai.sc.edu.projetomaria.service.CargaService;
+//
+//class HU1CargaCanalTest {
+// 	
+//	@Test		
+//	void testeInsert() throws SQLException {		
+//		ClassLoader classLoader	= getClass().getClassLoader();
+//		CargaService service = new CargaService();
+//		Path p = null;
+//		try {
+//			p = Paths.get(classLoader.getResource("dataset/carga_canal.csv").toURI());
+//		} catch (URISyntaxException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//			
+//		}
+//		service.cargaCanal(p);
+//		fail("falhou");
+//		
+//	}
+//	
+//	
+// }	
