7497bb5974c27891c47a032cca3ed74b1d8158ae
unknown <vinicius_paniz@estudante.sc.senai.br>
2018-11-29 21:00:21 -0200

    Add validation of the Exceptions By Alexsandro Santos and Vinicius

--- src/main/java/br/senai/sc/edu/projetomaria/dao/ProdutoDAO.java
+++ src/main/java/br/senai/sc/edu/projetomaria/dao/ProdutoDAO.java
@@ -132,20 +132,16 @@ public class ProdutoDAO extends AbstractDAO {
 	}
 
 	public int[] upsertSkuPhase(List<Phase> skuPhase) {
-		
 		String sql = "";
 		int successes = 0;
 		int[] resultados = { 0, 0 };
 		total = 0;
-		
 		for (Phase p : skuPhase) {
 			sql = "INSERT INTO sku_phase(" + "SKU_PHASE_IN," + "SKU_PHASE_OUT) VALUES (\" + p.getSkuNew() + \",\"\r\n"
-					+ "					+ p.getSkuOld() + \");\" "
-					+ "ON DUPLICATE KEY UPDATE SKU_PHASE_IN = ?, SKU_PHASE_OUT = ?";
+					+ "	+ p.getSkuOld() + \");\" " + "ON DUPLICATE KEY UPDATE SKU_PHASE_IN = ?, SKU_PHASE_OUT = ?";
 			try (Connection conn = getConnection(); PreparedStatement stmt = conn.prepareStatement(sql);) {
 				stmt.execute();
 				int retorno = stmt.executeUpdate(sql);
-				
 				if (retorno == 1) {
 					resultados[1] += 1;
 					resultados[0] = resultados[0] + 1;
@@ -154,6 +150,18 @@ public class ProdutoDAO extends AbstractDAO {
 				}
 				successes++;
 			} catch (SQLException e) {
+				if (e.getErrorCode() == 1062) {
+					LOGGER.info("registros duplicados. Retire-os e tente novamente. Mensagem SQL = " + e.getMessage());
+				}
+				if (e.getErrorCode() == 1) {
+					LOGGER.info("linha em branco. Ajuste e tente novamente. Mensagem SQL = " + e.getMessage());
+				}
+				if (e.getErrorCode() == 2) {
+					LOGGER.info("Coluna em branco. Ajuste e tente novamente. Mensagem SQL = " + e.getMessage());
+				} else {
+					LOGGER.info(
+							"Registro fora do Padrão. Retire-os e tente novamente. Mensagem SQL = " + e.getMessage());
+				}
 				LOGGER.debug(e);
 				throw new DAOLayerException(e);
 			}
