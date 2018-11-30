b3bdb5410ff08509570611793ae46afee58742cc
alexsantosfilho <devalexsandro@gmail.com>
2018-11-30 15:12:10 -0200

    Merge of the upserts

--- src/main/java/br/senai/sc/edu/projetomaria/dao/ProdutoDAO.java
+++ src/main/java/br/senai/sc/edu/projetomaria/dao/ProdutoDAO.java
@@ -50,7 +50,7 @@ public class ProdutoDAO extends AbstractDAO {
 	}
 
 	public List<Produto> exportarProdutos() {
-		String sql = "SELECT * FROM PRODUTO;";
+		String sql = "SELECT * FROM produto ORDER BY NOME_PRODUTO;";
 
 		List<Produto> p = new ArrayList<>();
 		try (Connection conn = getConnection();
@@ -129,6 +129,36 @@ public class ProdutoDAO extends AbstractDAO {
 			total++;
 		}
 		LOGGER.info(successes + " de " + total + " " + Messages.SUCCESS_PRODUTO);
+	}
+
+	public int[] upsert(List<Produto> produto) {
+		String sql = "INSERT INTO produto (COD_FAMILIA_COMERCIAL,NOME_PRODUTO,SKU) VALUES (?,?,?)"
+				+ "ON DUPLICATE KEY UPDATE COD_FAMILIA_COMERCIAL = ?, NOME_PRODUTO = ?, SKU = ?";
+		;
+		int[] resultados = new int[2];
+
+		try (Connection conn = getConnection(); PreparedStatement stmt = conn.prepareStatement(sql);) {
+			for (Produto p : produto) {
+				stmt.setInt(1, p.getIdComercial());
+				stmt.setString(2, p.getDescricao());
+				stmt.setInt(3, p.getSku());
+				stmt.setInt(4, p.getIdComercial());
+				stmt.setString(5, p.getDescricao());
+				stmt.setInt(6, p.getSku());
+				LOGGER.debug(stmt);
+				int retorno = stmt.executeUpdate();
+				if (retorno == 1) {
+					resultados[0] = resultados[0] + 1;
+				} else {
+					resultados[1] = resultados[0] + 1;
+				}
+
+			}
+		} catch (SQLException e) {
+			LOGGER.error(e);
+			throw new DAOLayerException(e);
+		}
+		return resultados;
 	}
 
 	public int[] upsertSkuPhase(List<Phase> skuPhase) {
