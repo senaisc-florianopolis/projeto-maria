186943185ffd47aa499c37c135016007205c79d2
Luciano Antonio Costa <16106846+la-costa@users.noreply.github.com>
2018-11-30 11:34:27 -0200

    Update HU4CargaFamiliaTest.java

--- src/test/java/HU4CargaFamiliaTest.java
+++ src/test/java/HU4CargaFamiliaTest.java
@@ -4,15 +4,8 @@ import org.junit.jupiter.api.Test;
 
 class HU4CargaFamiliaTest {
 
-	/**
-	 * Teste de historia de usario bdd 3
-	 * 
-	 * @author Yuri Mansani <yurimv9@gmail.com>
-	 * 
-	 *  
-	 */
 	@Test
-	void bdd3() {
+	void test() {
 		fail("Not yet implemented");
 	}
 
