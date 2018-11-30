e91dc5f18ffdbda21d390bd9b102fea2fe6a7a95
Robson <robccorreia@gmail.com>
2018-11-29 22:20:38 -0200

    teste feito

--- src/main/java/br/senai/sc/edu/projetomaria/io/CanalReader.java
+++ src/main/java/br/senai/sc/edu/projetomaria/io/CanalReader.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
@@ -24,17 +23,21 @@ public class CanalReader {
 	private static final String ID_CANAL = "ID_CANAL";
 	private static final String DESCRICAO = "DESCRICAO";
 	
+
 	public CanalReader(Path path) {
 		this.path = path;
 	}
 
 	public List<Canal> readCanal() {
+		
 		ArrayList<Canal> list = new ArrayList<>();
-		try(BufferedReader br = Files.newBufferedReader(this.path)){
-			Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader(ID_CANAL,DESCRICAO).withDelimiter(Config.CSV_DELIMITADOR).parse(br);
+		try (BufferedReader br = Files.newBufferedReader(this.path)) {
+			Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader(ID_CANAL, DESCRICAO)
+					.withDelimiter(Config.CSV_DELIMITADOR).withFirstRecordAsHeader().parse(br);
 			for (CSVRecord csvRecord : records) {
 				String idCanal = csvRecord.get(ID_CANAL);
 				String descricao = csvRecord.get(DESCRICAO);
+				LOGGER.debug("TESTE '" + idCanal + "' ");
 				Canal canal = new Canal();
 				canal.setId(Integer.parseInt(idCanal));
 				canal.setDescricao(descricao);
@@ -43,7 +46,7 @@ public class CanalReader {
 		} catch (IOException e) {
 			LOGGER.error(e);
 			throw new IOLayerException("", e);
-		} 
+		}
 
 		return list;
 	}
--- src/test/java/HU1CargaCanalTest.java
+++ src/test/java/HU1CargaCanalTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.awt.List;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,12 +11,10 @@ import br.senai.sc.edu.projetomaria.service.CargaService;
 import br.senai.sc.edu.projetomaria.service.ServiceResponse;
 import br.senai.sc.edu.projetomaria.service.ServiceResponse.STATUS;
 
-class HU1CargaCanalTest {
-
-	static CargaService service = null;
+public class HU1CargaCanalTest {
 	
 	@Test
-	static void testeInsert() {
+	void testeInsert() {
 		ClassLoader classLoader = HU1CargaCanalTest.class.getClassLoader();
 		Path p = null;
 		try {
@@ -23,8 +22,13 @@ class HU1CargaCanalTest {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
+		CargaService service = null;
 		service = new CargaService();
 		ServiceResponse s = service.cargaCanal(p);
 		assertEquals(s.getStatus(), STATUS.OK);
+		int[] response = (int[]) s.getResponse();
+		assertEquals(response[0], 4);
+		assertEquals(response[1], 0);
+		
 	}
 }
