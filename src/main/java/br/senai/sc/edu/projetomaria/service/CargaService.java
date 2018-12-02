7202708a6460738c7a5c053be5e884c5de9a369d
Antonio Moura <33243814+Antonniomoura@users.noreply.github.com>
2018-12-02 09:24:29 -0200

    Upsert Canais (#37)
    Implementação do upsert de Canais por @robcorreia @Antonniomoura @alexandrebdesouza1

--- src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
+++ src/main/java/br/senai/sc/edu/projetomaria/service/CargaService.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import br.senai.sc.edu.projetomaria.dao.CanalDAO;
 import br.senai.sc.edu.projetomaria.dao.ProdutoDAO;
 import br.senai.sc.edu.projetomaria.exception.DAOLayerException;
 import br.senai.sc.edu.projetomaria.exception.ServiceLayerException;
+import br.senai.sc.edu.projetomaria.io.CanalReader;
 import br.senai.sc.edu.projetomaria.io.ProdutoReader;
+import br.senai.sc.edu.projetomaria.model.Canal;
 import br.senai.sc.edu.projetomaria.model.Produto;
 import br.senai.sc.edu.projetomaria.resource.Messages;
 
@@ -17,7 +20,16 @@ public class CargaService {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	public ServiceResponse cargaCanal(Path path) {
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
+
+		CanalReader canalReader = new CanalReader(path);
+		CanalDAO canalDao = new CanalDAO();
+		List<Canal> canais = canalReader.readCanal();
+		int[] result = canalDao.upsertCanal(canais);
+		
+		ServiceResponse serviceResponse = new ServiceResponse(ServiceResponse.STATUS.OK, result);
+
+		return serviceResponse;
+
 	}
 
 	public ServiceResponse cargaFamilia(Path path) {
