a7d105ee7cc9d051a478835ed31b18319034be89
andre_hespanhol <andre_hespanhol@estudante.sc.senai.br>
2018-12-04 20:16:37 -0200

    AtualizaÃÃo do teste

--- src/test/java/HU3CargaProdutoTest.java
+++ src/test/java/HU3CargaProdutoTest.java
@@ -1,12 +1,43 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
+
+import br.senai.sc.edu.projetomaria.service.CargaService;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse;
 
 class HU3CargaProdutoTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		service = new CargaService();
+		ClassLoader classLoader = HU3CargaProdutoTest.class.getClassLoader();
+		Path p = null;
+		Path a = null;
+
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_familia_upsert.csv").toURI());
+			a = Paths.get(classLoader.getResource("dataset/carga_produto_upsert.csv").toURI());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		CargaService service = new CargaService();
+		ServiceResponse response = service.cargaFamilia(p, a);
+		ServiceResponse response2 = service.cargaProduto(p, a);
+
+		assertEquals(response.getStatus(), ServiceResponse.STATUS.OK);
+
+		if (a == null) {
+			System.out.println(ServiceResponse.STATUS.ERROR);
+		}
+		if (a != null) {
+			System.out.println(ServiceResponse.STATUS.OK);
+		}
+
 	}
 
 }
