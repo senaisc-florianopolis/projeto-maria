470f07d9a06b1a5ef6ee41d06c0b0246cae918c0
João Daniel <joao_frutuoso@estudante.sc.senai.br>
2018-12-04 22:01:04 -0200

    Teste HU2CargaHistorico

--- src/test/java/HU2CargaHistoricoTest.java
+++ src/test/java/HU2CargaHistoricoTest.java
@@ -1,12 +1,60 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import br.senai.sc.edu.projetomaria.service.CargaService;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse.STATUS;
+
 class HU2CargaHistoricoTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	static CargaService service = null;
+	
+	@BeforeAll
+	
+	static void beforeAll() {
+		ClassLoader classLoader = HU2CargaHistoricoTest.class.getClassLoader();		
+		Path q = null;
+		Path r = null;
+		
+		try {
+			q = Paths.get(classLoader.getResource("dataset/carga_canal_upsert.csv").toURI());
+			r = Paths.get(classLoader.getResource("dataset/carga_produto_upsert.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service = new CargaService();	
+		
+		service.cargaCanal(q);
+		service.cargaProduto(r);		
 	}
-
-}
+	
+	
+	@Test
+	void upsert() {
+		ClassLoader classLoader = HU2CargaHistoricoTest.class.getClassLoader();
+		Path p = null;
+		
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_historico_upsert.csv").toURI());			
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		service = new CargaService();
+		//service.cargaHistorico(p);
+		ServiceResponse s = service.cargaHistorico(p);
+		assertEquals(s.getStatus(), STATUS.OK);
+		int[] response = (int[]) s.getResponse();
+		assertEquals(response[0], 4);
+		assertEquals(response[1], 0);
+		
+		ServiceResponse x = service.cargaHistorico(p);
+		assertEquals(x.getStatus(), STATUS.OK);
+		int[] response1 = (int[]) x.getResponse();
+		assertEquals(response1[0], 0);
+		assertEquals(response1[1], 4);
+	}
+}	
