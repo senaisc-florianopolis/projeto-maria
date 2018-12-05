5a8171dd1563be1198a6f5cba1a8be4014e8e237
danielPrandi <daniel_prando@estudante.sc.senai.br>
2018-12-05 20:28:25 -0200

    Criando test.

--- src/test/java/HU7RelatorioProdutoTest.java
+++ src/test/java/HU7RelatorioProdutoTest.java
@@ -1,12 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.List;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
-class HU7RelatorioProdutoTest {
+import br.senai.sc.edu.projetomaria.service.CargaService;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse.STATUS;
 
+public class HU7RelatorioProdutoTest {
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testeInsert() {
+		ClassLoader classLoader = HU1CargaCanalTest.class.getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_produto.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		CargaService service = null;
+		service = new CargaService();
+		ServiceResponse s = service.cargaCanal(p);
+		assertEquals(s.getStatus(), STATUS.OK);
+		int[] response = (int[]) s.getResponse();
+		assertEquals(response[0], 4);
+		assertEquals(response[1], 0);
+		
 	}
-
 }
