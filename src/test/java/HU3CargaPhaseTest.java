f0dd46c883c72c333ba3b37581e5f9ce3ca15f13
alexsantosfilho <devalexsandro@gmail.com>
2018-12-10 14:02:25 -0200

    Update of test phase

--- src/test/java/HU3CargaPhaseTest.java
+++ src/test/java/HU3CargaPhaseTest.java
@@ -9,9 +9,13 @@ import org.junit.jupiter.api.Test;
 
 import br.senai.sc.edu.projetomaria.service.CargaService;
 import br.senai.sc.edu.projetomaria.service.ServiceResponse;
-import br.senai.sc.edu.projetomaria.service.ServiceResponse.STATUS;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 class HU3CargaPhaseTest {
+
+	static final Logger LOGGER = LogManager.getLogger();
 
 	static CargaService service = null;
 	static ClassLoader classLoader = HU3CargaPhaseTest.class.getClassLoader();
@@ -44,10 +48,24 @@ class HU3CargaPhaseTest {
 			e.printStackTrace();
 		}
 
-		service = new CargaService();
-		service.cargaPhase(p);
-		ServiceResponse s = service.cargaPhase(p);
-		assertEquals(s.getStatus(), STATUS.OK);
+		CargaService service = new CargaService();
+		ServiceResponse testin = service.cargaPhase(p);
+
+		assertEquals(testin.getStatus(), ServiceResponse.STATUS.OK);
+
+		int[] response = (int[]) testin.getResponse();
+
+		assertEquals(response[0], 9);
+		assertEquals(response[1], 0);
+
+		ServiceResponse test = service.cargaProduto(p);
+
+		assertEquals(test.getStatus(), ServiceResponse.STATUS.OK);
+
+		int[] response2 = (int[]) test.getResponse();
+		LOGGER.debug(response2);
+		assertEquals(response2[0], 0);
+		assertEquals(response2[1], 9);
 
 	}
 
