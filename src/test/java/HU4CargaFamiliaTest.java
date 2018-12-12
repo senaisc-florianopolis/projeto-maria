658e8962f051c8890e74aef40cb418aafd0d9026
Thiago Sus <thiago_sus@estudante.sc.senai.br>
2018-12-12 20:12:49 -0200

    Testes

--- src/test/java/HU4CargaFamiliaTest.java
+++ src/test/java/HU4CargaFamiliaTest.java
@@ -1,12 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
+
+import br.senai.sc.edu.projetomaria.service.CargaService;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse;
+import br.senai.sc.edu.projetomaria.service.ServiceResponse.STATUS;
 
 class HU4CargaFamiliaTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void testeInsert() {
+		ClassLoader classLoader = HU4CargaFamiliaTest.class.getClassLoader();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_familia.csv").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		CargaService service = null;
+		service = new CargaService();
+		ServiceResponse s = service.cargaFamilia(p);
+		assertEquals(s.getStatus(), STATUS.OK);
+		int[] response = (int[]) s.getResponse();
+		assertEquals(response[0], 0);
+		assertEquals(response[1], 5);
 
+	}
 }
